a38f63b848b13ca61033eb5dee4cfe1a1a8516f1
Sergey Skorokhod <skorokhod.sergey@gmail.com>
2017-12-31 17:30:50 +0300

    refactoring for test scenario

--- tests/test_scenario.cpp
+++ tests/test_scenario.cpp
@@ -1,3 +1,4 @@
+#include <memory>
 #include <gtest/gtest.h>
 #include "raft/Raft.h"
 #include "raft/Log.h"
@@ -7,26 +8,22 @@ using namespace raft;
 
 TEST(TestScenario, leader_appears)
 {
-    std::vector<raft::Server> r;
+    std::vector<std::shared_ptr<raft::Server>> r;
     Exchanger sender;
     Saver saver;
 
     const std::size_t Count = 3;
     for (std::size_t i = 0; i < Count; ++i)
     {
-        r.emplace_back(raft::Server(raft::NodeId(i), true, nullptr, &saver));
-        raft::Server& rx = r.back();
+        r.emplace_back(std::make_shared<raft::Server>(raft::NodeId(i), true, nullptr, &saver));
+        raft::Server& rx = *r.back();
 
         for(std::size_t j = 1; j < Count; ++j)
         {
             rx.nodes().add_node(raft::NodeId((i + j) % Count), true);
         }
         rx.timer().set_timeout(std::chrono::milliseconds(100), 5);
-    }
-
-    for (std::size_t i = 0; i < 3; ++i)
-    {
-        sender.add(&r[i]);
+        sender.add(&rx);
     }
 
     /* NOTE: important for 1st node to send vote request before others */
@@ -40,7 +37,7 @@ one_more_time:
 
         for (std::size_t j = 0; j < 3; j++)
         {
-            std::cout << " (" << r[j].get_current_term() << ", "<< (int)r[j].get_state()<< ")";
+            std::cout << " (" << r[j]->get_current_term() << ", "<< (int)r[j]->get_state()<< ")";
             sender.poll_msgs(raft::NodeId(j));
         }
         std::cout << std::endl;
@@ -50,12 +47,12 @@ one_more_time:
                 goto one_more_time;
 
         for (std::size_t j = 0; j < 3; j++)
-            r[j].raft_periodic(std::chrono::milliseconds(100));
+            r[j]->raft_periodic(std::chrono::milliseconds(100));
     }
 
     int leaders = 0;
     for (std::size_t j = 0; j < 3; j++)
-        if (r[j].is_leader())
+        if (r[j]->is_leader())
             leaders += 1;
 
     EXPECT_EQ(1, leaders);
